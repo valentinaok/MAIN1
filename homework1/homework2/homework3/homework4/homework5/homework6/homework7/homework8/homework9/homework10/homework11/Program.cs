f18e006d9d27240e39ae69d7defe6777c1f76d2b
valya <user@mail.ru>
2022-09-20 15:03:52 +0300

    add homework11

---
+++ homework1/homework2/homework3/homework4/homework5/homework6/homework7/homework8/homework9/homework10/homework11/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine ("Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;");
+Console.WriteLine ("значения b1, k1, b2 и k2 задаются пользователем.");
+Console.WriteLine("input b1:");
+double b1=Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("input k1:");
+double k1=Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("input b2:");
+double b2=Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("input k2:");
+double k2=Convert.ToDouble(Console.ReadLine());
+double xIntersection=0;
+double yIntersection=0;
+if (b1==b2 & k1==k2) Console.WriteLine("Прямые совпадают");
+else if (k1==k2) Console.WriteLine("Прямые параллельны друг другу");
+else 
+{
+    xIntersection =(b2-b1)/(k1-k2);
+    yIntersection =k1*xIntersection+b1;
+}
+    {Console.WriteLine("точка пересечения "+" ("+ xIntersection+" ;"+yIntersection+ ")");
+}
+  
